a70b26ded81a4b49c76381da309f3ff1d4e5b6fd
Ian <ian.githae2@student.moringaschool.com>
2025-03-25 11:52:36 +0300

    Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => cats.push(name);
+const destructivelyPrependCat = (name) => cats.unshift(name);
+const destructivelyRemoveLastCat = () => cats.pop();
+const destructivelyRemoveFirstCat = () => cats.shift();
+const appendCat = (name) => [...cats, name];
+const prependCat = (name) => [name, ...cats];
+const removeLastCat = () => cats.slice(0, -1);
+const removeFirstCat = () => cats.slice(1);
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat
+};
+
